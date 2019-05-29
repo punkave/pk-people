838d5b88037d61153e0e41461f984d08626b0ec1
bgantick <brian@punkave.com>
2019-05-29 15:53:35 -0400

    Begin schema, etc

--- index.js
+++ index.js
@@ -10,10 +10,45 @@ module.exports = {
   beforeConstruct: (self, options) => {
     options.addFields = [
       {
+        name: 'firstName',
+        label: 'First Name',
+        type: 'string'
+      },
+      {
+        name: 'lastName',
+        label: 'Last Name',
+        type: 'string'
+      },
+      {
         name: 'title',
         label: 'Full Name',
+        type: 'string',
+        readOnly: true
+      },
+      {
+        name: 'position',
+        label: 'Position/Job Title',
         type: 'string'
+      },
+      // TODO: photo - how do we allow this to be
+      // overridden project level
+      {
+        name: 'email',
+        label: 'Email',
+        type: 'string'
+      },
+      {
+        name: 'tel',
+        label: 'Telephone Number',
+        type: 'string'
+      },
+      {
+        name: 'summary',
+        label: 'Summary/Bio',
+        type: 'string',
+        textarea: true
       }
     ].concat(options.addFields || []);
+    // TODO: arrange fields
   }
 };
---
+++ public/js/editor-modal.js
@@ -0,0 +1,6 @@
+apos.define('pk-people-editor-modal', {
+  extend: 'apostrophe-pieces-editor-modal',
+  construct: (self, options) => {
+    // TODO: logic to populate title from first/last names
+  }
+});
