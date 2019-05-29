861c4314bffbdeb778489320180f67e482f44667
Nora Granahan <nora@punkave.com>
2019-05-29 16:57:38 -0400

    Starts validation for title field

--- public/js/editor-modal.js
+++ public/js/editor-modal.js
@@ -1,7 +1,18 @@
 apos.define('pk-people-editor-modal', {
   extend: 'apostrophe-pieces-editor-modal',
   construct: (self, options) => {
-    // TODO: Validation for title field before save
+    // TODO: Finish validation for title field before save
+    const superBeforeSaveContent = self.saveContent;
+    self.saveContent = (callback) => {
+      return superBeforeSaveContent((err) => {
+        if (!self.$title.val().length) {
+          console.log('no title field. dont save this piece');
+          return callback(err);
+        }
+        console.log('yay! saving content!');
+        return callback(null);
+      });
+    };
 
     const superBeforeShow = self.beforeShow;
     self.beforeShow = (callback) => {
